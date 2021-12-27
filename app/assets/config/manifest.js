a01cc4efdc8d6887a8c370adfb82cb2f545ae811
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-12-27 12:48:12 +0000

    Serve node_modules images via rails asset pipeline
    This should mean we can take advantage of `rails assets:precompile`
    which will do the usual checksumming thing to avoid issues with cache
    invalidation, and should mean we don't have to ship node_modules to
    production.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,9 @@
 //= link_tree ../images
 //= link_tree ../builds
+
+//= link images/govuk-mask-icon.svg
+//= link images/govuk-apple-touch-icon-180x180.png
+//= link images/govuk-apple-touch-icon-167x167.png
+//= link images/govuk-apple-touch-icon-152x152.png
+//= link images/govuk-apple-touch-icon.png
+//= link images/govuk-opengraph-image.png
